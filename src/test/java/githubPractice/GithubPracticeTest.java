8d006141eb25f6cc655fa36eff388f63eae89a55
Jharana <jharanakusahu@gmail.com>
2026-01-02 16:45:00 +0530

    first comite

---
+++ src/test/java/githubPractice/GithubPracticeTest.java
@@ -0,0 +1,13 @@
+package githubPractice;
+
+import org.testng.annotations.Test;
+
+public class GithubPracticeTest {
+	@Test
+	public void githubPracticeTest() {
+		System.out.println("hii Github");
+		String data=System.getProperty("smarty");
+		System.out.println(data);
+	}
+
+}
---
+++ src/test/java/jenkinsPractice/JenkinsPracticeTest.java
@@ -0,0 +1,10 @@
+package jenkinsPractice;
+
+import org.testng.annotations.Test;
+
+public class JenkinsPracticeTest {
+@Test
+public void jenkinsPracticeTest() {
+	System.out.println("hii jenkinsPractice");
+}
+}
